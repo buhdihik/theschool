87813157744108dd2a50e88532fe22da08c2c217
ishe <ishe@192.168.2.1>
2017-06-09 23:25:47 +0800

    commit from home

---
+++ public_html/wp-content/themes/theSchool/index.php
@@ -0,0 +1,16 @@
+<?php get_header();?>
+        <div class="container-fluid ">
+            <div class="row courseBG " id="second">
+								<div class="col-md-5"></div>
+								<div class="col-md-7" style="margin-top:80px;">
+									<h1>Jump in<br/>
+								   to our courses offered!
+								</h1>
+								<h2>and make new friends!</h2>
+								<button class="btn btn-info">Enroll Today!</button>
+								<button class="btn btn-success">Show Courses!</button>
+								</div>
+								
+            </div>
+        </div>
+<?php get_footer();?>
