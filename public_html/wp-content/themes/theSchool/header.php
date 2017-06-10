72c41fa91d314c4c3954d31d37f225773583abe5
ishe <ishe@192.168.1.28>
2017-06-10 10:45:54 +0800

    commited at store

--- public_html/wp-content/themes/theSchool/header.php
+++ public_html/wp-content/themes/theSchool/header.php
@@ -40,7 +40,8 @@
       </head>
       <body>
         <div class="container-fluid indexBG" id="first">
-             <nav class="navbar navbar-inverse navbar-fixed-top navbarBG-erase">
+             <nav class="navbar navbar-inverse navbar-fixed-top">
+              
         <div class="container-fluid">
           <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
