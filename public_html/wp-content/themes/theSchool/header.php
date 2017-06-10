72965fc6f8fdd45f28d9544a78b755c5a017180d
ishe <ishe@192.168.1.28>
2017-06-10 12:42:45 +0800

    commited at store

--- public_html/wp-content/themes/theSchool/header.php
+++ public_html/wp-content/themes/theSchool/header.php
@@ -7,7 +7,7 @@
         <title><?php wp_title('|', true, 'right'); ?></title>
         <?php wp_head(); ?>
 	
-   <link rel="stylesheet" type="text/css" href="http://www.ishedorene.com/wp-content/themes/theSchool/css/main.css" media="screen">
+   <link rel="stylesheet" type="text/css" href="http://www.ishedorene.com/theschool/wp-content/themes/theSchool/css/main.css" media="screen">
    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Cinzel|Fjalla+One|Pontano+Sans" media="screen">
    
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -55,7 +55,7 @@
           <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
               <li class="active"><a href="#first">Home</a></li>
-              <li><a href="#second">Course</a></li>
+              <li><a href="#second">Course2</a></li>
               <li><a href="#">Price/Rates</a></li>
               <li><a href="#">Coaches</a> </li>
               <li><a href="#">About</a></li>
